32673ac3bc6c7a53da6a8fc9dbf1b937cdf97b10
CauaHenrique19 <cauah6002@gmail.com>
2021-11-14 20:32:59 -0300

    insert dependency of user repository

--- backend/src/useCases/CreateFriendship/index.ts
+++ backend/src/useCases/CreateFriendship/index.ts
@@ -1,9 +1,11 @@
 import { FriendshipRepository } from "../../Repositories/FriendshipRepository/FriendshipRepository";
+import { UserRepository } from "../../Repositories/UserRepository/UserRepository";
 import { CreateFriendshipController } from "./CreateFriendshipController";
 import { CreateFriendshipUseCase } from "./CreateFriendshipUseCase";
 
 const friendshipRepository = new FriendshipRepository()
-const createFriendshipUseCase = new CreateFriendshipUseCase(friendshipRepository)
+const userRepository = new UserRepository()
+const createFriendshipUseCase = new CreateFriendshipUseCase(friendshipRepository, userRepository)
 const createFriendshipController = new CreateFriendshipController(createFriendshipUseCase)
 
 export { createFriendshipController }
